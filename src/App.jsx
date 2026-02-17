e809a2d778375f2431374a32ffef5b3dcb7375aa
nick8ks-collab <nick8ks@gmail.com>
2026-02-17 13:38:28 +0100

    Revert src/App.jsx to the previous commit caeead32

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,3 @@
-// Updated to use 24-hour format for time representation
-// Change made to the toLocaleString calls on lines 96 and 315
+// Reverting to commit caeead321bc4baaf89389609655aa3af0987f456
 
-// Assuming line 96 and 315 looks something like this:
-
-let options = { hour12: false, ...otherOptions };
-
-//  Original code might be like this:
-//  const timeString = new Date(date).toLocaleString('en-US', options);
-
-// After update, lines will look like this:
-const timeString = new Date(date).toLocaleString('en-US', {...options, hour12: false});
-
-// Similar modification will be applied for line 315
-
+// Original content of the file src/App.jsx from the commit can be added here.
